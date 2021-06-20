7f0e387218f70f06fda7b7dfd6e582b2c53efcd3
uzairriaz <uzairriaz.pk@gmail.com>
2021-06-20 16:03:21 +0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Umair Riaz on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
